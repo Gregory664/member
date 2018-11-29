039c13582f1e86138a5cb7e118d77382695d4638
Gregory664 <gregory664@yandex.ru>
2018-11-29 21:11:23 +0300

    Добавлен основной интерфейс MemberLogic

---
+++ src/main/java/ru/src/logic/interfaces/MemberLogic.java
@@ -0,0 +1,11 @@
+package ru.src.logic.interfaces;
+
+import javafx.collections.ObservableList;
+import ru.src.model.Member;
+
+public interface MemberLogic {
+    static void addMember(Member member) { }
+    static void removeMember(Member member) { }
+    static void updateMember(Member member) { }
+
+}
