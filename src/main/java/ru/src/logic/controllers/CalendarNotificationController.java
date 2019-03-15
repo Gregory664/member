9fb0be28629cc133c46f3f7d57fa1c4f7e3751e4
Gregory664 <gregory664@yandex.ru>
2019-03-15 17:06:11 +0300

    CalendarNotificationController.java was refactored.

--- src/main/java/ru/src/logic/controllers/CalendarNotificationController.java
+++ src/main/java/ru/src/logic/controllers/CalendarNotificationController.java
@@ -1,11 +1,16 @@
 package ru.src.logic.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import ru.src.logic.implementation.DBConnection;
 import ru.src.logic.implementation.MemberUtils;
+
+import java.io.IOException;
 
 public class CalendarNotificationController {
     @FXML
@@ -17,28 +22,39 @@ public class CalendarNotificationController {
     @FXML
     public Label label_organizationBirthday_count;
 
-    private Stage calendarStage;
+    private FXMLLoader calendarFXMLLoader = new FXMLLoader();
+    private Parent calendar;
     private CalendarController calendarController;
-
+    private Stage calendarStage;
 
     @FXML
     public void initialize() {
         label_directorBirthday_count.setText(DBConnection.getCountOfDirectorBirthdayToday().toString());
         label_organizationBirthday_count.setText(DBConnection.getCountOfOrganizationBirthdayToday().toString());
+
+        initializeCalendar();
     }
 
-    public void setCalendarParams(Stage calendarStage, CalendarController calendarController) {
-        this.calendarStage = calendarStage;
-        this.calendarController = calendarController;
-    }
+    private void initializeCalendar() {
+        try {
+            calendarFXMLLoader.setLocation(getClass().getResource("/ui/Calendar.fxml"));
+            calendar = calendarFXMLLoader.load();
+            calendarController = calendarFXMLLoader.getController();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        calendarStage = new Stage();
+        calendarStage.setScene(new Scene(calendar));
+        calendarStage.setTitle("Календарь");
+    }
     @FXML
-    public void openDirectorCalendar(MouseEvent mouseEvent) {
+    public void openDirectorCalendar() {
         calendarController.setCurrentMonth(0);
         calendarStage.show();
     }
     @FXML
-    public void openOrganizationCalendar(MouseEvent mouseEvent) {
+    public void openOrganizationCalendar() {
         calendarController.setCurrentMonth(1);
         calendarStage.show();
     }
